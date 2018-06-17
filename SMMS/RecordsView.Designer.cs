48a5ef6399c84d78a8463364af0b43ac7a73c4db
naizicqc <910985309@qq.com>
2018-06-17 22:30:49 +0800

    销售记录查询

--- SMMS/RecordsView.Designer.cs
+++ SMMS/RecordsView.Designer.cs
@@ -31,19 +31,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.UserID = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.VIPID = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.Date = new System.Windows.Forms.DateTimePicker();
-            this.label4 = new System.Windows.Forms.Label();
-            this.Good = new System.Windows.Forms.TextBox();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.ConfirmBtn = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Good = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.VIPID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Date2 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,8 +65,12 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.Date2);
+            this.groupBox1.Controls.Add(this.ClearBtn);
+            this.groupBox1.Controls.Add(this.ConfirmBtn);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.Good);
             this.groupBox1.Controls.Add(this.label4);
@@ -76,45 +80,52 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.UserID);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(699, 223);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "设置筛选";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // label2
+            // ClearBtn
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "会员ID：";
+            this.ClearBtn.Location = new System.Drawing.Point(575, 24);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(90, 33);
+            this.ClearBtn.TabIndex = 10;
+            this.ClearBtn.Text = "清空";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
             // 
-            // VIPID
+            // ConfirmBtn
             // 
-            this.VIPID.Location = new System.Drawing.Point(148, 74);
-            this.VIPID.Name = "VIPID";
-            this.VIPID.Size = new System.Drawing.Size(199, 25);
-            this.VIPID.TabIndex = 3;
+            this.ConfirmBtn.Location = new System.Drawing.Point(575, 172);
+            this.ConfirmBtn.Name = "ConfirmBtn";
+            this.ConfirmBtn.Size = new System.Drawing.Size(90, 25);
+            this.ConfirmBtn.TabIndex = 9;
+            this.ConfirmBtn.Text = "确认";
+            this.ConfirmBtn.UseVisualStyleBackColor = true;
+            this.ConfirmBtn.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label3
+            // comboBox1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 127);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 15);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "日期：";
+            this.comboBox1.Font = new System.Drawing.Font("SimSun", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "商品编号",
+            "商品名称"});
+            this.comboBox1.Location = new System.Drawing.Point(352, 172);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(96, 26);
+            this.comboBox1.TabIndex = 8;
             // 
-            // Date
+            // Good
             // 
-            this.Date.Location = new System.Drawing.Point(147, 120);
-            this.Date.Name = "Date";
-            this.Date.Size = new System.Drawing.Size(200, 25);
-            this.Date.TabIndex = 5;
+            this.Good.Location = new System.Drawing.Point(147, 172);
+            this.Good.Name = "Good";
+            this.Good.Size = new System.Drawing.Size(199, 25);
+            this.Good.TabIndex = 7;
             // 
             // label4
             // 
@@ -125,76 +136,86 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "商品：";
             // 
-            // Good
+            // Date
             // 
-            this.Good.Location = new System.Drawing.Point(147, 172);
-            this.Good.Name = "Good";
-            this.Good.Size = new System.Drawing.Size(199, 25);
-            this.Good.TabIndex = 7;
+            this.Date.CustomFormat = "\"yyyy-MM-dd\"";
+            this.Date.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.Date.Location = new System.Drawing.Point(147, 120);
+            this.Date.Name = "Date";
+            this.Date.ShowCheckBox = true;
+            this.Date.Size = new System.Drawing.Size(200, 25);
+            this.Date.TabIndex = 5;
             // 
-            // comboBox1
+            // label3
             // 
-            this.comboBox1.Font = new System.Drawing.Font("SimSun", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "商品名称",
-            "商品编号"});
-            this.comboBox1.Location = new System.Drawing.Point(352, 172);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(96, 26);
-            this.comboBox1.TabIndex = 8;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "日期：";
             // 
-            // groupBox2
+            // VIPID
             // 
-            this.groupBox2.Controls.Add(this.dataGridView1);
-            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.groupBox2.Location = new System.Drawing.Point(0, 204);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(699, 364);
-            this.groupBox2.TabIndex = 9;
-            this.groupBox2.TabStop = false;
+            this.VIPID.Location = new System.Drawing.Point(148, 74);
+            this.VIPID.Name = "VIPID";
+            this.VIPID.Size = new System.Drawing.Size(199, 25);
+            this.VIPID.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "会员ID：";
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 21);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 229);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(693, 340);
+            this.dataGridView1.Size = new System.Drawing.Size(693, 336);
             this.dataGridView1.TabIndex = 0;
             // 
-            // button1
+            // Date2
             // 
-            this.button1.Location = new System.Drawing.Point(575, 172);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 25);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "确认";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Date2.CustomFormat = "\"yyyy-MM-dd\"";
+            this.Date2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.Date2.Location = new System.Drawing.Point(377, 120);
+            this.Date2.Name = "Date2";
+            this.Date2.ShowCheckBox = true;
+            this.Date2.Size = new System.Drawing.Size(200, 25);
+            this.Date2.TabIndex = 11;
             // 
-            // button2
+            // label5
             // 
-            this.button2.Location = new System.Drawing.Point(575, 24);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 33);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "清空";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(349, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "到";
             // 
             // RecordsView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Name = "RecordsView";
             this.Size = new System.Drawing.Size(699, 568);
             this.Load += new System.EventHandler(this.RecordsView_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -205,8 +226,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox UserID;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.Button ConfirmBtn;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox Good;
         private System.Windows.Forms.Label label4;
@@ -214,7 +235,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox VIPID;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker Date2;
     }
 }
--- SMMS/RecordsView.cs
+++ SMMS/RecordsView.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace SMMS
 {
@@ -22,15 +23,110 @@ namespace SMMS
         private void RecordsView_Load(object sender, EventArgs e)
         {
             this.Dock = DockStyle.Fill;
+            this.Date.Format = DateTimePickerFormat.Custom;
+            this.Date.CustomFormat = "yyyy-MM-dd";
+            this.Date2.Format = DateTimePickerFormat.Custom;
+            this.Date2.CustomFormat = "yyyy-MM-dd";
+            ClearBtn_Click(null, null);
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {//确认查询
+            string sql = "select * from sales where ";
+            if (UserID.Text != "")
+            {
+                sql += "SalerID='" + UserID.Text + "' ";
+            }
+            if (VIPID.Text != "")
+            {
+                if (UserID.Text != "")
+                {
+                    sql += "and ";
+                }
+
+                sql += "VipID='" + VIPID.Text + "' ";
+            }
+            if (Good.Text != "")
+            {
+                if (UserID.Text != "" || VIPID.Text != "")
+                {
+                    sql += "and ";
+                }
+
+                if (comboBox1.SelectedIndex == 0)
+                {//ID
+                    sql += "GID = '" + Good.Text + "' ";
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {//名字
+                    sql+= "GName like '%" + Good.Text + "%' ";
+                }
+            }
+            if (Date.Checked == true && Date2.Checked == true)
+            {
+                if (UserID.Text != "" || VIPID.Text != "" || Good.Text != "")
+                {
+                    sql += "and ";
+                }
+                sql += "SDate between #" + Date.Value.ToShortDateString() + " 00:00:00# And #" + Date2.Value.ToShortDateString() + " 23:59:59# ";
+
+            }
+            if (UserID.Text == "" && 
+                VIPID.Text == "" &&
+                Date.Checked == false &&
+                Date2.Checked == false &&
+                Good.Text == "")
+            {
+                sql = "select * from sales";
+            }
+            if (MainForm._instance.oleDb.State != ConnectionState.Open)
+            {
+                MainForm._instance.oleDb.Open();
+            }
+            OleDbDataAdapter dbDataAdapter = new OleDbDataAdapter(sql, MainForm._instance.oleDb); //创建适配对象
+            DataSet ds = new DataSet();
+            dbDataAdapter.Fill(ds, "sales");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "sales";
+
+            //dataGridView1.Columns[0].HeaderText = "工号";
+            //dataGridView1.Columns[1].HeaderText = "姓名";
+
+            MainForm._instance.oleDb.Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
         {
-            string sql;
-
-
-
 
         }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            this.UserID.Text = "";
+            this.VIPID.Text = "";
+            this.Date.Checked = false;
+            this.Date2.Checked = false;
+            this.Good.Text = "";
+            this.comboBox1.SelectedIndex = 0;
+
+            string sql = "select * from sales";
+            if (MainForm._instance.oleDb.State != ConnectionState.Open)
+            {
+                MainForm._instance.oleDb.Open();
+            }
+            OleDbDataAdapter dbDataAdapter = new OleDbDataAdapter(sql, MainForm._instance.oleDb); //创建适配对象
+            DataSet ds = new DataSet();
+            dbDataAdapter.Fill(ds, "sales");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "sales";
+
+            //dataGridView1.Columns[0].HeaderText = "工号";
+            //dataGridView1.Columns[1].HeaderText = "姓名";
+
+            MainForm._instance.oleDb.Close();
+        }
+
+
+
     }
 }
